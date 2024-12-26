e8d42e7f2e9e53c53e576a92a94b481e45b68093
Sofia <asofimp@gmail.com>
2024-12-26 12:43:35 -0600

    feat: first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import FAB from "./components/FAB";
 
 export default function App() {
+  const [counter, setCounter] = useState(10);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{counter}</Text>
+
+      <FAB
+        label="+1"
+        onPress={() => setCounter(counter + 1)}
+        onLongPress={() => setCounter(0)}
+        position="right"
+      />
+
+      <FAB label="Reset" onPress={() => setCounter(0)} position="left" />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: "100",
   },
 });
